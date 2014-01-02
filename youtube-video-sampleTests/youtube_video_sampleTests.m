5328d6f8929055d08e84c10277b4f5fccceb8dc9
Hector Carrasco <htcarrasc@gmail.com>
2014-01-02 01:49:34 -0300

    commit — 3

---
+++ youtube-video-sampleTests/youtube_video_sampleTests.m
@@ -0,0 +1,34 @@
+//
+//  youtube_video_sampleTests.m
+//  youtube-video-sampleTests
+//
+//  Created by Hector Carrasco on 08-12-13.
+//  Copyright (c) 2013 Hector Carrasco. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface youtube_video_sampleTests : XCTestCase
+
+@end
+
+@implementation youtube_video_sampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
