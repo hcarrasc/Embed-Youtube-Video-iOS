43764fff8134c854593e70c573bd3c52e828b0d4
Hector Carrasco <htcarrasc@gmail.com>
2014-01-02 01:53:03 -0300

    commit — 4 : updating viewcontroler.m

--- youtube-video-sample/ViewController.m
+++ youtube-video-sample/ViewController.m
@@ -20,12 +20,10 @@
 {
     [super viewDidLoad];
 
-    NSString *html = [NSString stringWithFormat:@"<html><body><iframe type=\"application/x-shockwave-flash\" data=\"http://www.chilevision.cl/jwplayer/jwplayer.flash.swf\" width=\"%f\" height=\"%d\" bgcolor=\"#000000\" id=\"senal_online\" name=\"senal_online\" tabindex=\"0\" style=\"position: absolute;\"><param name=\"allowfullscreen\" value=\"true\"><param name=\"allowscriptaccess\" value=\"always\"><param name=\"seamlesstabbing\" value=\"true\"><param name=\"wmode\" value=\"opaque\"></iframe></body></html>", [[UIScreen mainScreen] bounds].size.width-30, 150];
-    [myWebView loadHTMLString:html baseURL:nil];
-/*
+
      NSString *html = [NSString stringWithFormat:@"<html><body><iframe class=\"youtube-player\" type=\"text/html\" width=\"%f\" height=\"%d\" src=\"http://www.youtube.com/embed/Ur4OEBZOAXs?HD=1;rel=0;showinfo=1\" allowfullscreen frameborder=\"0\" rel=nofollow></iframe></body></html>", [[UIScreen mainScreen] bounds].size.width-30, 150];
      [myWebView loadHTMLString:html baseURL:nil];
-*/
+
 }
 
 - (void)didReceiveMemoryWarning
